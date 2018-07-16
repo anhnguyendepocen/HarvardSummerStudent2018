209f8dee5b4cb7090a476a95e74718830d46ce6d
kwartler <ehk116@gmail.com>
2018-07-16 10:28:06 -0400

    welcome

---
+++ welcome.R
@@ -0,0 +1,8 @@
+# Welcome to your vitrual private server for Data Mining for Business.
+# I will turn off this server at the conclusion of class so please make sure 
+# any material you want to keep from the course has been downloaded.
+# Please sign up at github.com and perform a git PULL to get the lessons, 
+# course materials and data from our shared repository.
+# 
+# 
+# -Prof. Kwartler
