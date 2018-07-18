9e1dfa45e9d767a6d656720010bbc1957a3f410d
Ludovic Henry <ludovic@xamarin.com>
2018-07-18 16:40:17 -0400

    [homework] Day 3

--- lessons/2_July17-DataPrep/day2_homework/day2_airfareEDA.R
+++ lessons/2_July17-DataPrep/day2_homework/day2_airfareEDA.R
@@ -5,48 +5,60 @@
 #' Instructions: Add your name above, fill out the answers below by adding code, results or values per below.  Save the file with your first and last name in the file title as in Ted_Kwartler_Day2_Homework.R. Once finished email your script file.
 
 # wd
-
+setwd('~/HarvardSummerStudent2018/lessons/2_July17-DataPrep/day2_homework')
 
 # Libs: radiant.data, DataExplorer
+library('radiant.data')
+library('DataExplorer')
 
 # Data
-airfare <- read.csv(_____)
+airfare <- read.csv('Airfares.csv')
+airfare$E_CODE[airfare$E_CODE=='*'] <- NA
+airfare$S_CODE[airfare$S_CODE=='*'] <- NA
 
 ### Perform EDA 
 # Review the top 6 rows
-____(airfare)
+head(airfare)
 
 # Get the summary stats
-____(airfare)
+summary(airfare)
 
 # What is the dimensions of the data frame?
-___(airfare)
+dim(airfare)
 
 # What is the number of starting cities? 
-nlevels(____)
+nlevels(airfare$S_CITY)
 
 # What is the number of ending cities?
-___(airfare$____)
+nlevels(airfare$E_CITY)
 
 # Is there a relationship between starting city population and ending city
-cor(____, ____   )
+cor(airfare$S_POP, airfare$E_POP)
 
 # Visualize the relationship between distance & fare.  Use the base function for a scatterplot.
+ggplot(airfare) +
+  geom_point(aes(DISTANCE, FARE))
 
 # Whats the average fare?
+mean(airfare$FARE)
 
 # Whats the min fare?
+min(airfare$FARE)
 
 # Whats the max fare?
+max(airfare$FARE)
 
 # Calculate the range(), max() - min() of fares
-___(___) #OR
-___(___) - ___(___)
+range(airfare$FARE) #OR
+max(airfare$FARE) - min(airfare$FARE)
 
 # Using library(DataExplorer) create a plot of the data structure; navigate the visual to examine variable types
+DataExplorer::create_report(airfare)
 
 # Create a plot to see % of missing records by variable
+DataExplorer::plot_missing(airfare)
 
 # Using radiant.data, create 2 additional visuals exploring distrubtions & relationships.  Click on the *very* tiny download icon in the upper left of the visual to download a copy and send with the script.
+radiant.data()
 
 # End
--- lessons/2_July17-DataPrep/day2_homework/day2_diabetesPrep.R
+++ lessons/2_July17-DataPrep/day2_homework/day2_diabetesPrep.R
@@ -5,53 +5,59 @@
 #' Instructions: Add your name above, fill out the answers below by adding code, results or values per below.  Save the file with your first and last name in the file title as in Ted_Kwartler_Day2_Homework.R. Once finished email your script file.
 
 # wd
-
+setwd('~/HarvardSummerStudent2018/lessons/2_July17-DataPrep/day2_homework')
 
 # Libs: DataExplorer, vtreat
+library('DataExplorer')
+library('vtreat')
 
 # Data
-diabetes <- read.csv(_____)
+diabetes <- read.csv('10k_diabetesAdj.csv')
 
 # Get all classes
 sapply(diabetes, class)
 
 ### Perform EDA 
 # Review the top 6 rows and first 9 columns
-diabetes[ _:_, _:_]
+diabetes[ 1:6, 1:9]
 
 # Get the summary stats
-____(diabetes)
+summary(diabetes)
 
 # What is the dimensions of the data frame?
-___(diabetes)
+dim(diabetes)
 
 # Using the data frame `wentHome` what is the cross tally between:
 # discharge_disposition_id and readmitted
 wentHome <- subset(diabetes, diabetes$discharge_disposition_id=='Discharged to home')
 
-table(droplevels(______$________),
-      droplevels(as.factor(______$______)))
+table(droplevels(wentHome$discharge_disposition_id),
+      droplevels(as.factor(wentHome$readmitted)))
 
 # Whats the average num_lab_procedures?
+mean(diabetes$num_lab_procedures)
 
 # Whats the min num_medications?
+min(diabetes$num_medications)
 
 # What does the histogram of time_in_hospital look like? 
+DataExplorer::plot_histogram(diabetes$time_in_hospital)
 
 # Using library(DataExplorer) Create a plot to see % of missing records by variable
+DataExplorer::plot_missing(diabetes)
 
 # Remove the variable recommended by DataExplorer visual.
-____$_____ <- NULL
+diabetes$weight <- NULL
 
 # Remove the text
 
 ## Now design a treatment plan to ready this data for modeling
-plan <- designTreatmentsC(______, _____(______)[1:43],'readmitted', 'FALSE')
+plan <- designTreatmentsC(diabetes, colnames(diabetes)[1:43],'readmitted', 'FALSE')
 
 # Prepare the data
-treatedDiabetes <- ____(____,____)
+treatedDiabetes <- prepare(plan, diabetes)
 
 # Save a copy to the folder as 'treatedDiabetes.csv'
-write.csv(____,_____, row.names=F)
+write.csv(treatedDiabetes, 'treatedDiabetes.csv', row.names=F)
 
 # End
